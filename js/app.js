86ffa25ea2476cada1e825ab3ee63a4c12a90800
re-z <re-z@ukr.net>
2019-02-02 21:20:42 +0200

    added prompt validation

--- js/app.js
+++ js/app.js
@@ -3,11 +3,32 @@ let totalBudget = document.querySelector('#total'),
     totalLeft = document.querySelector('#left'),
     allExpenses = document.querySelector('.list-group'),
     form = document.querySelector('#add-expense')
-    formSubmitBtn = document.querySelector('#formSubmit');
+    formSubmitBtn = document.querySelector('#formSubmit'),
+    errorField = document.querySelector('#form-error'),
+    successField = document.querySelector('#form-success');
 
 
 //functions
-let userBudget = prompt('How much do you plan to spend at this week???', 'print a number, please');
+
+let userBudget = false;
+function checkPrompt(){
+    userBudget = prompt('How much do you plan to spend at this week???', 'print a number, please');
+    //check if prompt fielad is filled and contains number
+    if(userBudget != null && userBudget.length > 0 && !isNaN(userBudget)) {return true}
+    else {userBudget = false};
+}
+// if all good = go forward, else = show prompt again
+while (userBudget == false) {
+    checkPrompt();
+}
+// let userBudget = prompt('How much do you plan to spend at this week???', 'print a number, please');
+// alert(userBudget.length)
+
+// if(userBudget.length > 0) {
+//     for(let userBudget= prompt('How much do you plan to spend at this week???', 'print a number, please'); userBudget.length > 0; ) {
+//         let userBudget = prompt('How much do you plan to spend at this week???', 'print a number, please');
+//     }
+// // }
 totalBudget.innerHTML = userBudget;
 totalLeft.innerHTML = userBudget;
 formSubmitBtn.addEventListener('click', function (evt) {
@@ -22,6 +43,11 @@ formSubmitBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
 
     if(expenseName && amount) {
+        //hide error
+        errorField.style.display = 'none';
+
+
+
         //create template for budget itema and add it to the DOM
         itemPattern = `
             <li class="list-group-item expenses__item">${expenseName}<span class="expenses__amount">$${amount}</span></li>
@@ -30,7 +56,12 @@ formSubmitBtn.addEventListener('click', function (evt) {
         form.reset();
         //convert string to number and show left budget in DOM
         totalLeft.innerHTML = parseInt(userBudget) - parseInt(amount);
-        
+        //show success field
+        successField.style.display = 'block'
+        // hide success field after 3 seconds
+        setTimeout(function(){
+            successField.style.display = 'none'
+        }, 3000)
         if((parseInt(userBudget)*0.5) > parseInt(totalLeft.innerHTML) ) {
             totalleftWrap.classList.remove('alert-success');
             totalleftWrap.classList.add('alert-warning');
@@ -41,7 +72,7 @@ formSubmitBtn.addEventListener('click', function (evt) {
         }
 
     } else {
-        alert(1)
+        errorField.style.display = 'block';
     }
     
 })
